e45184a8c6a8500acb41aa0414a69196eaf491b2
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-31 22:49:16 -0400

    Implementando as retricoes de movimento

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -24,6 +24,27 @@ namespace tabuleiro
             QteMovimentos++;
         }
 
+        public bool ExisteMovimentoPossiveis()
+        {
+            bool[,] mat = MovimentosPossiveis();
+            for (int i = 0; i < Tab.Linha; i++)
+            {
+                for (int j = 0; j < Tab.Coluna; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
         public abstract bool[,] MovimentosPossiveis();
         
 
