7e8a0d214d16bf0619a4f47b7813b8812a7722cc
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-05 16:26:48 -0400

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -142,6 +142,19 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = Tab.Peca(destino);
+            //#jogadaespecial Promoção
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) ||(p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    PecasHs.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    PecasHs.Add(dama);
+                }
+            }
 
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
@@ -160,7 +173,7 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-            Peca p = Tab.Peca(destino);
+            
 
             //#Jogadaespecial en passant
             if(p is Peao && (destino.Linha == origem.Linha -2 || destino.Linha == origem.Linha + 2))
